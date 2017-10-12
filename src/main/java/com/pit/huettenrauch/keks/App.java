8d0a321030548045efaa3bbf09462c4aefa46a5a
PitHuettenrauch <huettenrauch.parimeo@gmail.com>
2017-10-12 14:41:14 +0200

    Initial commit

---
+++ src/main/java/com/pit/huettenrauch/keks/App.java
@@ -0,0 +1,13 @@
+package com.pit.huettenrauch.keks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
